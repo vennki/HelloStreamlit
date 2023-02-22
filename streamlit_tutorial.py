d52fc4bd299d397ccaaf45f6b1a2c544e21d37fa
vennki <vennki9@gmail.com>
2023-02-22 17:28:16 +0530

    Update streamlit_tutorial.py

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
-import plotly.express as px
+#import plotly.graph_objects as go
+#import plotly.express as px
 import numpy as np
 import altair as alt
 
